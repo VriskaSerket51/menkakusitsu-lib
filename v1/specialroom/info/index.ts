98017ed0184fd3980b5cf15fd72262579a31732b
vriskaserket51 <serketvriska51@gmail.com>
2022-10-17 14:24:17 +0900

    1.0.8 commit

--- v1/specialroom/info/index.ts
+++ v1/specialroom/info/index.ts
@@ -14,3 +14,49 @@ export interface PutInfoRequest {
 export interface PutInfoResponse extends DefaultResponse {
     information: SpecialroomInfo;
 }
+
+export interface LocationInfo {
+    id: number;
+    value: string;
+}
+
+export interface GetLocationInfoRequest {}
+
+export interface GetLocationInfoResponse extends DefaultResponse {
+    locationInfo: LocationInfo[];
+}
+
+export interface PurposeInfo {
+    id: number;
+    value: string;
+}
+
+export interface GetPurposeInfoRequest {}
+
+export interface GetPurposeInfoResponse extends DefaultResponse {
+    purposeInfo: PurposeInfo[];
+}
+
+export interface StudentInfo {
+    uid: number;
+    value: string;
+    name: string;
+}
+
+export interface GetStudentInfoRequest {}
+
+export interface GetStudentInfoResponse extends DefaultResponse {
+    studentInfo: StudentInfo[];
+}
+
+export interface TeacherInfo {
+    uid: number;
+    value: string;
+    name: string;
+}
+
+export interface GetTeacherInfoRequest {}
+
+export interface GetTeacherInfoResponse extends DefaultResponse {
+    teacherInfo: TeacherInfo[];
+}
