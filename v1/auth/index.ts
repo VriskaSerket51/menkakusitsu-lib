a1f4671d8de2bbe5117b1fe25385e6b3ede0e551
vriskaserket51 <serketvriska51@gmail.com>
2022-10-31 15:31:22 +0900

    로그인 콜백 추가

--- v1/auth/index.ts
+++ v1/auth/index.ts
@@ -1,25 +1,23 @@
 import { DefaultResponse } from "../..";
 
 export interface PostLoginRequest {
-    id: string,
-    password: string,
-};
+    id: string;
+    password: string;
+}
 
 export interface PostLoginResponse extends DefaultResponse {
-    accessToken: string,
-    refreshToken: string,
-};
+    accessToken: string;
+    refreshToken: string;
+    callbacks?: string[];
+}
 
-export interface DeleteLogoutRequest {
-};
+export interface DeleteLogoutRequest {}
 
-export interface DeleteLogoutResponse extends DefaultResponse {
-};
+export interface DeleteLogoutResponse extends DefaultResponse {}
 
-export interface PostRefreshRequest {
-};
+export interface PostRefreshRequest {}
 
 export interface PostRefreshResponse extends DefaultResponse {
-    accessToken: string,
-    refreshToken: string,
-};
+    accessToken: string;
+    refreshToken: string;
+}
