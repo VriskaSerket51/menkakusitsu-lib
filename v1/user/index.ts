fd4e8006e9292f078db1711a2df55a9213d1359b
vriskaserket51 <serketvriska51@gmail.com>
2022-10-31 14:34:08 +0900

    개인정보 변경 api 추가

--- v1/user/index.ts
+++ v1/user/index.ts
@@ -6,4 +6,37 @@ export interface UserInfo {
     name: string;
 }
 
+export interface UserPrivateInfo {
+    uid?: number;
+    sid?: number;
+    id?: string;
+    name?: string;
+    email?: string;
+    password?: string;
+}
+
+export interface GetMyPrivateInfoRequest {}
+
+export interface GetMyPrivateInfoResponse extends DefaultResponse {
+    private: UserPrivateInfo;
+}
+
+export interface PutEmailRequest {
+    oldEmail: string;
+    newEmail: string;
+}
+
+export interface PutEmailResponse extends DefaultResponse {
+    newEmail: string;
+}
+
+export interface PutPasswordRequest {
+    oldPassword: string;
+    newPassword: string;
+}
+
+export interface PutPasswordResponse extends DefaultResponse {
+    newPassword: string;
+}
+
 export * from "./push";
