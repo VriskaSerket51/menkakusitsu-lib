df7a0e18fcd3fd38d3de13648aa661baf4b2f821
vriskaserket51 <serketvriska51@gmail.com>
2023-01-14 12:37:56 +0900

    Permission enum 및 UserInfo type 추가

--- index.ts
+++ index.ts
@@ -1,4 +1,17 @@
 export interface DefaultResponse {
-    status: number,
-    message: string,
+    status: number;
+    message: string;
+}
+
+export enum Permission {
+    Guest = 0,
+    Student = 1,
+    Teacher = 2,
+    Dev = 100,
+}
+
+export type UserInfo = {
+    uid: number;
+    id: string;
+    permission: number;
 };
