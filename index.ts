82fc6dd90ea0d756672675647462013136af5c2b
vriskaserket51 <serketvriska51@gmail.com>
2023-01-14 13:14:09 +0900

    add other jwt default fields

--- index.ts
+++ index.ts
@@ -14,6 +14,10 @@ export class TokenPayload {
     public uid: number = 0;
     public id: string = "";
     public permission: number = 0;
+    public type: string = "";
+    public iat: number = 0;
+    public exp: number = 0;
+    public jti: string = "";
 
     hasPermission(permission: number) {
         return this.permission >= permission;
