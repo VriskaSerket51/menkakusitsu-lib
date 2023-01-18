875514721d19037469af0e8a89f75df9e709f5af
sunsh9876543210 <sunsh9876543210@gmail.com>
2023-01-18 22:29:25 +0900

    vmcreate config change

--- lib/v1/vm/index.ts
+++ lib/v1/vm/index.ts
@@ -1,10 +1,49 @@
-export interface VmConfig {
+import { DefaultResponse } from "../..";
+
+export interface PostCreateVmRequest {
+    required: CreateVmConfig;
+    optional: VmConfig;
+}
+
+export interface PostCreateVmResponse extends DefaultResponse {
+    vmId: number;
+    config: {
+        config0: CreateVmConfig;
+        config1: VmConfig;
+    }
+}
+
+export interface RemoveVM {
     id: number;
-    name: string;
-    core: number;
-    ramSize: number; //MiB
-    balloon: number;
-    diskController: string;
-    diskSize: number;
-    customArgs: string[];
+}
+
+export interface CreateVmConfig {
+    id?: number;
+    core?: number;
+    ramSize?: number;
+    diskSize?: number;
+}
+export interface VmConfig {
+    name?: string;
+    acpi?: boolean;
+    core?: number;
+    cputype?: string;
+    ramSize?: number; //MiB
+    diskController?: string;
+    customArgs?: string[];
+    audio?: string;
+    autostart?: boolean;
+    bios: string;
+    kvm?: boolean;
+    machine?: string;
+    net?: string;
+    ostype?: string;
+    tablet?: boolean;
+    vga?: string;
+    vncport?: number;
+}
+
+export interface GetGpuAttach {
+    id: number;
+    type?: string;
 }
