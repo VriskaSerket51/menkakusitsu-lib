a68c5fdff6c13959fbd7c5733aa74c1243d7afcf
jordaeni13 <70104240+jordaeni13@users.noreply.github.com>
2023-03-09 21:05:03 +0900

    tried to implement outer, 03, 09, 2023

--- lib/v1/specialroom/index.ts
+++ lib/v1/specialroom/index.ts
@@ -11,6 +11,18 @@ export interface SpecialroomInfo {
     when: number,
 };
 
+export interface OuterInfo {
+    applyId: number,
+    state: number,
+    master: UserInfo,
+    teacher: UserInfo,
+    applicants: string,
+    location: string,
+    purpose: string,
+    when: number,
+};
+
+
 export * from "./apply";
 export * from "./attendance";
 export * from "./info";
